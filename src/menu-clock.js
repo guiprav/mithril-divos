fd0d2274114f3f2f631bac4f434ea03c3b602b5c
Guilherme Vieira <super.driver.512@gmail.com>
2017-12-24 04:30:24 -0200

    WIP: Menu clock improvements.

--- src/menu-clock.js
+++ src/menu-clock.js
@@ -1,13 +1,4 @@
-function leftPad(ch, len, str) {
-  str = str.toString();
-  len -= str.length;
-
-  while (len-- > 0) {
-    str = ch + str;
-  }
-
-  return str;
-}
+import moment from 'moment';
 
 export class MenuClock {
   constructor() {
@@ -22,12 +13,7 @@ export class MenuClock {
   }
 
   update() {
-    let now = new Date();
-
-    let hh = leftPad(0, 2, now.getHours());
-    let mm = leftPad(0, 2, now.getMinutes());
-    let ss = leftPad(0, 2, now.getSeconds());
-
-    this.label = `${hh}:${mm}:${ss}`;
+    this.date = moment().format('ddd DD MMM');
+    this.time = moment().format('hh:mm');
   }
 }
